97539a03952d2d6303ac94a6b960315461c95ba8
Naoru347 <joshua.paiz@gmail.com>
2023-05-11 16:42:04 -0400

    Figured out BERTopic visualization quick...work in a damn notebook

--- topic_modeling.py
+++ topic_modeling.py
@@ -1,35 +1,45 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 import json
-import os
-import glob
 import time
 from bertopic import BERTopic
 
-#Load the data file by reading from the JSON into a generic data object
+# Load the data file by reading from the JSON into a generic data object
 print("Importing data and building DataFrame:")
 start_time = time.time()
-with open ('data/combinedData.json', 'r') as f:
+with open('data/combinedData.json', 'r') as f:
     data = json.load(f)
 end_time = time.time()
 load_time = end_time - start_time
 print("Data load time", load_time)
-#Convert the data to a PANDAS DataFrame for additional processing
+print(data[0])
+
+# Convert the data to a PANDAS DataFrame for additional processing
 print("Data loaded, converting to DataFrame")
 start_time = time.time()
 df = pd.DataFrame(data)
 end_time = time.time()
 convert_time = end_time - start_time
-print("Data successfully converted to DataFrame. Conversion time: ", convert_time)
-#Confirm loading and Conversion
+print("Data successfully converted to DataFrame. Conversion time:", convert_time)
+
+# Confirm loading and conversion
 print("Data loaded, generating head summary:")
 print(df.head())
 column_names = df.columns.to_list()
 print(column_names)
-#Begin topic modeling by creating the model
-descriptions = df["description"].to_list()
-model = BERTopic(verbose=True)
-topics, probabilities = model.fit_transform(descriptions)
 
-#Get list of topics
-print(model.get_topic_freq().head(20))
+# Begin topic modeling by creating the model
+model = BERTopic(embedding_model="all-MiniLM-L6-v2")
+docs = df.description.to_list()
+topics, probabilities = model.fit_transform(docs)
+model.get_topic_info()
+
+print(model.get_topic(1))
+print(model.get_topic(2))
+print(model.get_topic(3))
+print(model.get_topic(4))
+print(model.get_topic(5))
+
+# # Visualizations
+model.visualize_topics()
+model.visualize_barchart()
